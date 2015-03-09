3d778065a5f3e635546715289fa41a25c00f9ad2
Eric Tsang <surplus.et@gmail.com>
2015-03-09 15:16:12 -0700

    modified main to start the GPS server; its not a test echo servr anymore
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,68 +1,97 @@
 package main;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.Socket;
-import java.net.SocketAddress;
 
-import lib.TCPServer;
+import domain.AndroidClientServer;
+import domain.GpsRecordManager;
+import domain.WebClientServer;
 
 public class Main
 {
-    public static void main(String[] args)
+    public static final String USAGE = "usage: java "+Main.class.getName()
+            +" [android_svr_port] [websocket_svr_port]";
+
+    public static void main(String[] args) throws IOException, InterruptedException
     {
-        MyTCPServer svr;
+        int androidServerPort;
+        int websocketServerPort;
+        AndroidClientServer androidSvr;
+        WebClientServer webSockSvr;
+        GpsRecordManager gpsRecords;
+
+        // parse command line arguments
         try
         {
-            svr = new MyTCPServer(7000);
+            androidServerPort = Integer.valueOf(args[0]);
+            websocketServerPort = Integer.valueOf(args[1]);
+        }
+        catch(Exception e)
+        {
+            System.out.println(USAGE);
+            return;
+        }
+
+        // set up the servers
+        try
+        {
+            gpsRecords = new GpsRecordManager();
+            androidSvr = new AndroidClientServer(androidServerPort,gpsRecords);
+            webSockSvr = new WebClientServer(websocketServerPort,gpsRecords);
         }
         catch (IOException e)
         {
             throw new RuntimeException(e);
         }
-        System.out.println("Server started on port 7000");
-        svr.start();
-    }
 
-    public static class MyTCPServer extends TCPServer
-    {
+        // start the servers
+        androidSvr.start();
+        webSockSvr.start();
 
-        public MyTCPServer(int serverPort) throws IOException
-        {
-            super(serverPort);
-        }
-
-        @Override
-        protected void onClose(Socket conn, boolean remote)
-        {
-            SocketAddress addr = conn.getRemoteSocketAddress();
-            System.out.println("client "+addr+" disconnected "+(remote?"by remote":"by server")+".");
-            
-        }
-
-        @Override
-        protected void onMessage(Socket conn, String msg)
-        {
-            SocketAddress addr = conn.getRemoteSocketAddress();
-            System.out.println(addr+": "+msg);
-//            stop();
-
-            try
-            {
-                DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-                os.writeUTF("SEE YA LATER");
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        protected void onOpen(Socket conn)
-        {
-            SocketAddress addr = conn.getRemoteSocketAddress();
-            System.out.println("client "+addr+" has connected.");
-        }
+        // end the program when input is received
+        System.in.read();
+        androidSvr.stop();
+        webSockSvr.stop();
     }
 }
+//
+//    public static class MyTCPServer extends TCPServer
+//    {
+//
+//        public MyTCPServer(int serverPort) throws IOException
+//        {
+//            super(serverPort);
+//        }
+//
+//        @Override
+//        protected void onClose(Socket conn, boolean remote)
+//        {
+//            SocketAddress addr = conn.getRemoteSocketAddress();
+//            System.out.println("client "+addr+" disconnected "+(remote?"by remote":"by server")+".");
+//            
+//        }
+//
+//        @Override
+//        protected void onMessage(Socket conn, String msg)
+//        {
+//            SocketAddress addr = conn.getRemoteSocketAddress();
+//            System.out.println(addr+": "+msg);
+////            stop();
+//
+//            try
+//            {
+//                DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+//                os.writeUTF("SEE YA LATER");
+//            }
+//            catch (IOException e)
+//            {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        @Override
+//        protected void onOpen(Socket conn)
+//        {
+//            SocketAddress addr = conn.getRemoteSocketAddress();
+//            System.out.println("client "+addr+" has connected.");
+//        }
+//    }
